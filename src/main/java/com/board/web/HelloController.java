fc4d5f12cc4ba3db33a4e6a61d92508604c5a098
3comma <3comma.devmj@gmail.com>
2022-02-25 02:11:01 +0900

    te

--- src/main/java/com/board/web/HelloController.java
+++ src/main/java/com/board/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.board.web;
 
+import com.board.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // 컨트롤러를 JSON으로 반환해주는 어노테이션. @ResponseBody 안써두댐.
@@ -10,5 +12,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
     }
 }
